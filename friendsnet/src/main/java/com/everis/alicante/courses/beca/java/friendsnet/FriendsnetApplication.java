92d93027260c08afaf35fe1a71806f400c844cbc
everisbeca <everisbeca@ALC-EVERBECA>
2018-07-18 17:10:08 +0200

    Incorporo el Swagger en el paquete principal --> friendsnetApplication(class)

--- friendsnet/src/main/java/com/everis/alicante/courses/beca/java/friendsnet/FriendsnetApplication.java
+++ friendsnet/src/main/java/com/everis/alicante/courses/beca/java/friendsnet/FriendsnetApplication.java
@@ -2,6 +2,14 @@ package com.everis.alicante.courses.beca.java.friendsnet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
@@ -11,5 +19,17 @@ public class FriendsnetApplication {
 		SpringApplication.run(FriendsnetApplication.class, args);
 	}
 	
+	@Configuration
+	@EnableSwagger2
+	public class SwaggerConfig {                                    
+	   @Bean
+	   public Docket api() {
+	       return new Docket(DocumentationType.SWAGGER_2)  
+	         .select()                                  
+	          .apis(RequestHandlerSelectors.basePackage("com.everis.alicante.courses.beca.java.friendsnet"))
+	         .paths(PathSelectors.any())                          
+	         .build();                                          
+	   }
+	}
 }
 
